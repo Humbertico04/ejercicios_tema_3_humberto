abf7efb2ad1a51e5f421f32e55b0005e907b3ebf
Humbertico04 <humbertico2004@gmail.com>
2023-03-31 17:38:16 +0200

    clase matriz

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,2 +1,20 @@
 # Realiza el código para calcular el determinante de una matriz cuadrada de [3 x 3], regla de Sarrus de 
 # forma recursiva y de forma iterativa
+
+class Matriz:
+    def __init__(self, filas, columnas, matriz=None):
+        self.filas = filas
+        self.columnas = columnas
+        if matriz is None:
+            self.matriz = [[0 for _ in range(columnas)] for _ in range(filas)]
+        else:
+            self.matriz = matriz
+
+    def obtener_elemento(self, fila, columna):
+        return self.matriz[fila][columna]
+
+    def asignar_elemento(self, fila, columna, valor):
+        self.matriz[fila][columna] = valor
+
+    def __str__(self):
+        return str(self.matriz)
